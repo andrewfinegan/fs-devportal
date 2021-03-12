b24c7a7872301ccafca4d70916e688c3f5b9785a
Pavan Joshi <74169051+pavanjoshi12@users.noreply.github.com>
2021-03-12 10:30:11 -0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -15,18 +15,18 @@ func processError(err error) {
 
 type Config struct {
 	ContentPath struct {
-		ApiSpecYamlFile       string `yaml:"tenantProviderApiFile" envconfig:"API_SPEC_YAML_FILE"`
-		ProductLayoutFile     string `yaml:"productLayoutFile" envconfig:"PRODUCT_LAYOUT_FILE"`
-		TenantProviderApiFile string `yaml:"apiSpecYamlFile" envconfig:"TENANT_PROVIDER_API_FILE"`
+		ApiSpecYamlFile       string `yaml:"tenantProviderApiFile"`
+		ProductLayoutFile     string `yaml:"productLayoutFile"`
+		TenantProviderApiFile string `yaml:"apiSpecYamlFile"`
 	} `yaml:"contentpath"`
 
 	GitHub struct {
-		GitHubSourceOwner     string `yaml:"gitHubSourceOwner" envconfig:"GITHUB_SOURCE_OWNER"`
+		GitHubRawContentHost  string `yaml:"gitHubRawContentHost"`
+		GitHubSourceOwner     string `yaml:"gitHubSourceOwner"`
 		GitHubSourceRepo      string `yaml:"gitHubSourceRepo" envconfig:"GITHUB_SOURCE_REPO"`
 		GitHubContentBranch   string `yaml:"gitHubContentBranch" envconfig:"GITHUB_CONTENT_BRANCH"`
 		GitHubUserName        string `yaml:"gitHubUser" envconfig:"GITHUB_USER_NAME"`
 		GitHubAuthToken       string `yaml:"gitHubAuthToken" envconfig:"GITHUB_AUTH_TOKEN"`
-		GitHubRawContentHost  string `yaml:"gitHubRawContentHost" envconfig:"GITHUB_RAW_CONTENT_HOST"`
 		GitHubContentFullPath string
 	} `yaml:"github"`
 }
